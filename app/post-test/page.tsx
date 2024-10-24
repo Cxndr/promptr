5ea029cea47c1ba0808235286ec50aefa2687f66
Cxndr <livewellandmakethings@gmail.com>
2024-10-24 13:45:33 +0100

    fixed component folder location

---
+++ app/post-test/page.tsx
@@ -0,0 +1,12 @@
+import PostInput from "@/components/PostInput";
+
+
+
+export default function PostTest() {
+  return (
+    <div>
+      <h1>Post Test</h1>
+      <PostInput />
+    </div>
+  )
+}
---
+++ components/PostInput.tsx
@@ -0,0 +1,11 @@
+
+export default function PostInput() {
+
+
+  return (
+    <div>
+      <input type="text" placeholder="What's on your mind?" />
+      <button>Post</button>
+    </div>
+  )
+}
