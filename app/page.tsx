ef674cafe1a1d89f0f082ec36107d374d3a434df
Joe <joewint3r@gmail.com>
2024-10-26 21:09:25 +0100

    landing page adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import AniTitle from "@/components/ui/AniTitle"
 import Image from "next/image";
-import wordGameLogo from "@/public/img/wordGameLogo.webp";
+import wordGameLogo from "../public/img/wordGameLogo.webp";
 import { fetchUserProfile } from "@/lib/fetch";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -14,21 +14,24 @@ export default async function Home() {
     redirect("/profile");
   }
   return (
-    <div>
-      <Image
-        className="flex justify-center mt-16"
+    <>
+      <div className="flex absolute w-full h-40 justify-center items-start pt-8 top-8">
+        <Image
         src={wordGameLogo}
         alt="Black & white Art-deco logo of a person holding a card"
         width={100}
         height={38}
         priority
       />
-      <div className="">
+      </div>
+      
+      <div className="flex absolute w-full h-40 justify-center items-start pt-8 top-32 left-20">
         <AniTitle/>
       </div >
+
       <div className="absolute top-4 right-4 z-10">
         <ModeToggle />
       </div>
-    </div>
+    </>
   );
 }
