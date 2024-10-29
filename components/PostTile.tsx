42590e93ece707ef5419568d7d6b5be168d47c66
MangoSama1000 <tilija_nitej@hotmail.com>
2024-10-29 13:13:43 +0000

    nearly done edit

--- components/PostTile.tsx
+++ components/PostTile.tsx
@@ -4,21 +4,62 @@ import { Post } from "@/lib/types";
 // import { User } from "@clerk/nextjs/server";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Heart, Trash2, Pencil } from 'lucide-react';
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import PostInput from "./PostInput";
+import { useEffect } from "react";
+import { Word } from "@/lib/types";
+
+
+
 
 type PromptPostProps = {
   post: Post;
-  deletePost: (postId : number) => void
-  editPost: (postId : number) => void
+  deletePost: (postId: number) => void
+  editPost: (postId: number) => void
   ownedByUser: boolean
   timeAgoCreated: string
 }
 
-export default function PostTile({post, deletePost, editPost, ownedByUser, timeAgoCreated}:PromptPostProps) {
+export default function PostTile({ post, deletePost, editPost, ownedByUser, timeAgoCreated }: PromptPostProps) {
+  useEffect(() => {
+
+  })
+  const baseWords: Word[] = []
+  console.log('post.words',post.words)
+  console.log('post.slice',post.words.slice(0,20))
+  post.words.slice(0.20).map((baseWord) => {
+    baseWords.push({
+      word: baseWord,
+      type: 'base',
+      used: 0,
+    })
+  })
+  console.log('baseWords:',baseWords)
+
+  const fillerWords: Word[] = []
+  post.words.slice(20, post.words.length).map((fillerWord) => {
+    fillerWords.push({
+      word: fillerWord,
+      type: 'filler',
+      used: 0,
+    })
+  })
+  console.log('fillerwords:',fillerWords)
 
   function handleDelete() {
     if (post && post.id) {
-      deletePost(post.id); 
+      deletePost(post.id);
     }
   }
 
@@ -27,25 +68,27 @@ export default function PostTile({post, deletePost, editPost, ownedByUser, timeA
       editPost(post.id);
     }
   }
+  const handleUpdate = (data: { words: string[], content: string, }) => {
 
+  }
   return (
-    <div className="flex bg-zinc-800 bg-opacity-70 rounded-2xl p-0 overflow-hidden">``
-      
+    <div className="flex bg-zinc-800 bg-opacity-70 rounded-2xl p-0 overflow-hidden">
+
       <div className="flex flex-col justify-start items-center gap-4 relative">
         <Avatar className="w-40 h-40 relative">
-          <AvatarImage 
-            src={post.user.imageUrl || "/img/default-avatar.webp"} 
+          <AvatarImage
+            src={post.user.imageUrl || "/img/default-avatar.webp"}
             alt="User Profile Image"
             width={96}
             height={96}
             className="w-full h-full"
           />
           <AvatarFallback>
-            { post.user.username ? 
-              post.user.username.charAt(0).toUpperCase() 
+            {post.user.username ?
+              post.user.username.charAt(0).toUpperCase()
               : "?"
             }
-          </AvatarFallback> 
+          </AvatarFallback>
           <h3 className="bg-zinc-800 bg-opacity-70 p-1 absolute bottom-0 w-full font-bold">
             {post.user.username || "Unknown User"}
           </h3>
@@ -55,34 +98,52 @@ export default function PostTile({post, deletePost, editPost, ownedByUser, timeA
       <div className="relative h-full flex flex-col justify-center items-center flex-grow">
 
         <div className="absolute right-0 top-0 flex justify-center items-center">
-          { post.createdAt &&
+          {post.createdAt &&
             <div className="text-zinc-500 py-2 px-3">
               {timeAgoCreated}
             </div>
           }
-          { ownedByUser &&
+          {ownedByUser &&
             <div className="flex">
-              <Button
-                onClick={handleEdit} 
-                className="bg-zinc-50 bg-opacity-0 hover:bg-zinc-700"
-              >
-                <Pencil color={"white"}/>
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button
+                    onClick={handleEdit}
+                    className="bg-zinc-50 bg-opacity-0 hover:bg-zinc-700"
+                  >
+                    <Pencil color={"white"} />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit post</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your post here. Click save when you're done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <PostInput
+                    baseWords={baseWords}
+                    fillerWords={fillerWords}
+                    handleSubmit={handleUpdate}
+                  />
+                </DialogContent>
+              </Dialog>
               <Button
                 onClick={handleDelete}
                 className="bg-zinc-50 bg-opacity-0 hover:bg-red-800"
               >
-                <Trash2 color={"white"}/>
+                <Trash2 color={"white"} />
               </Button>
             </div>
           }
         </div>
 
+
         <p className="text-3xl p-8">
           {post.content}
         </p>
 
-          
+
         <div className="flex justify-center items-center gap-4 text-lg absolute right-6 bottom-2">
 
           <span className="flex gap-1 items-center">
@@ -99,7 +160,7 @@ export default function PostTile({post, deletePost, editPost, ownedByUser, timeA
             <span>🤮</span>
             <span>0</span>
           </span>
-          
+
           <span className="flex gap-1 items-center">
             <span>🙄</span>
             <span>0</span>
@@ -109,6 +170,7 @@ export default function PostTile({post, deletePost, editPost, ownedByUser, timeA
 
       </div>
 
-    </div>
+    </div >
   )
 }
+
