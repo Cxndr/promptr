28aceb516ab91ddce46062420403b91a01d34534
ScottGreenhalgh <scottgreenhalgh62@me.com>
2024-10-29 05:53:27 +0000

    dyslexic font toggle

---
+++ components/FontToggle.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function FontToggleClient() {
+  const [isDyslexic, setIsDyslexic] = useState(false);
+
+  const toggleFont = () => {
+    setIsDyslexic((prev) => {
+      const newValue = !prev;
+      localStorage.setItem("isDyslexic", JSON.stringify(newValue));
+      return newValue;
+    });
+  };
+
+  useEffect(() => {
+    // apply saved preference if in local storage
+    const savedPreference = JSON.parse(localStorage.getItem("isDyslexic") || "false");
+    if (savedPreference) {
+      setIsDyslexic(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    // update body class when isDyslexic state changes
+    if (isDyslexic) {
+      document.body.classList.add("font-dyslexic");
+      document.body.classList.remove("font-default");
+    } else {
+      document.body.classList.add("font-default");
+      document.body.classList.remove("font-dyslexic");
+    }
+  }, [isDyslexic]);
+
+  return (
+    <button className={"pl-4"} onClick={toggleFont}>
+      {isDyslexic ? "Default Font" : "Dyslexic Font"}
+    </button>
+  );
+}
