34cdb71e3c57037f549fa137e159c1c6e6a172c7
Joe <joewint3r@gmail.com>
2024-10-31 16:37:10 +0000

    mobile vp about us, prompts & home pages

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -9,7 +9,7 @@ export default async function PlayPage() {
   return (
     <>
       <Gravity wordsArr={wordsArr} />
-      <div className="max-w-7xl bg-background/80 py-8 my-auto m-4 flex flex-col gap-8 justify-center items-center rounded-3xl shadow-md shadow-black z-10">
+      <div className="w-[80svw] sm:w-[30svw] max-w-7xl bg-background/80 py-8 my-auto m-4 flex flex-col gap-8 justify-center items-center rounded-3xl shadow-md shadow-black z-10">
         <h1 className="text-2xl">Available Prompts</h1>
         {prompts.rows.map((prompt) => (
           <p key={prompt.id}>
